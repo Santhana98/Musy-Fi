1f3dd8894141741f30b68eeae1ab742c619bef06
Santhana kumar <129987602+Santhana98@users.noreply.github.com>
2026-06-13 16:33:51 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { Providers } from './providers';
+import Providers from '@/components/Providers';
 
 export const metadata: Metadata = {
   title: 'Musy-Fi',
